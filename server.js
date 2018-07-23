afbfb0fdcc6567d6fabf78f507cde65e365ee340
Dominic Coelho <domwork@live.com>
2018-07-23 15:08:45 +0100

    added logic to deal with js css txt ico html jpg png lol

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const port = 1337;
 
 const fs = require("fs");
 
+const path = require("path"); // Dom and Martin decided this is better for getting file ext
+
 server.listen(port, () => {
     console.log(`Server is live on port ${port}`)
 })
@@ -34,8 +36,7 @@ function handler(request, response){
             }
         });
         // response.end();
-    }
-    else if(endpoint == "/node"){
+    } else if(endpoint == "/node"){
         response.writeHead(200, {"Content-Type": "text/html"})
         response.write(`${message}... and on node!`)
         response.end();
@@ -44,9 +45,37 @@ function handler(request, response){
         response.write("<h1>Wooo! Go NodeGirls!</h1>")
         response.end();
     } else {
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write(message);
-        response.end()
+        // the below would cause issues if we've got several periods in the name. Gonna use path.extname instead
+        // const fileExt = file.split(".")[1]; 
+
+        const contentType = {
+            ".html": "text/html",
+            ".jpg": "image/jpg",
+            ".png": "image/png",
+            ".js": "application/javascript",
+            ".css": "text/css",
+            ".txt": "text",
+            ".ico": "image/x-icon",
+        }
+
+        console.log(path.extname(endpoint))
+        console.log(contentType[path.extname(endpoint)]);
+        response.writeHead(200, {"Content-Type": contentType[path.extname(endpoint)]});
+        fs.readFile(__dirname + "/public" + endpoint, (error, file) => {
+            if (error){
+                // console.log("error!")
+                // console.log(`${__dirname}public${endpoint}`);
+                response.writeHead(500, {"Content-Type": "text/html"});
+                response.write("Internal server error (500). Sorry!");
+                response.end();
+            } else {
+                // console.log("no error!")
+                // response.write(file);
+                response.end(file);
+            }
+        });
+
+        // response.end(endpoint);
     }
 
 }
