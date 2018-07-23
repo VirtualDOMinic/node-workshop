db7119712e965d3727908faf8e0951259493def3
Dominic Coelho <domwork@live.com>
2018-07-23 14:16:47 +0100

    first server response created/succeded

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer(handler);
 
 const port = 1337;
 
@@ -8,3 +8,11 @@ server.listen(port, () => {
     console.log(`Server is live on port ${port}`)
 })
 
+const message = "Hey! We are live"
+
+function handler(request, response){
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(message);
+    response.end()
+
+}
