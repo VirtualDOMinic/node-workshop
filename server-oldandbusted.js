a58e871eab6ee62ef088b8b58a415373de0aaacc
martingaston <squidman@gmail.com>
2018-07-24 12:31:07 +0100

    accidentally deleted alll files because of what thing said

---
+++ server-oldandbusted.js
@@ -0,0 +1,8 @@
+const http = require("http");
+const handler = require("./src/handler-oldandbusted");
+const server = http.createServer(handler);
+const port = 1337;
+
+server.listen(port, () => {
+  console.log(`Server is live on port ${port}`);
+});
--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-const handler = require("./src/handler");
-const server = http.createServer(handler);
-const port = 1337;
-
-server.listen(port, () => {
-  console.log(`Server is live on port ${port}`);
-});
